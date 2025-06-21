30f9e50afe4dc14635e070c5981bc45f5e531ac3
Kush Pareek <71487381+kushpareek@users.noreply.github.com>
2025-06-21 13:13:25 +0530

    Update email_utils.py

--- champion/email_utils.py
+++ champion/email_utils.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 def send_email(subject, body, to_email, from_email=settings.DEFAULT_FROM_EMAIL, attachment=None, attachment_name=None):
     configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key['api-key'] = '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'  # Securely use the API key from settings
+    configuration.api_key['api-key'] = ''  # Securely use the API key from settings
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
 
