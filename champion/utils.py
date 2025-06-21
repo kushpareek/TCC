8fe57966b725f93fa49fdb1c24b304cc30f4a397
Kush Pareek <71487381+kushpareek@users.noreply.github.com>
2025-06-21 13:12:20 +0530

    Update utils.py

--- champion/utils.py
+++ champion/utils.py
@@ -43,7 +43,7 @@ from sib_api_v3_sdk.rest import ApiException
 
 def send_email(subject, body, to_email, from_email=settings.DEFAULT_FROM_EMAIL, attachment=None, attachment_name=None):
     configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key['api-key'] = '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'
+    configuration.api_key['api-key'] = ""
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
 
